45870244ff51739a640899d723719474568d1737
kawan97 <pshtiwankawan@gmail.com>
2022-03-09 00:05:46 -0800

    use bootstrap and create navbar

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap-vue/dist/bootstrap-vue.css";
 
 createApp(App).use(store).use(router).mount('#app')
+import "bootstrap/dist/js/bootstrap.js";
